8dc38840042fdc4ada2ee64dbae8f46268c64962
bhurtelmahesh <bhurtelace@gmail.com>
2023-07-12 15:39:03 +0900

    Initial Commit

---
+++ SFML Template/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SFML Template
+//
+//  Created by Maheshwor Bhurtel on 2023/07/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
